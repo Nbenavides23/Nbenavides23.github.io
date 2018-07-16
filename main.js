f3830e80314160c57382a755d74949ca007d346f
Nbeanavides23 <franco.noly@gmail.com>
2018-07-16 15:18:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+class Pokemon {
+    constructor(hp,attack,defense,abilities){
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.abilities = abilities;
+    }
+}
+
+class Trainer {
+    constructor(name){
+        this.name = name;
+        this.pokemonchildren = []
+    }
+    
+    add(pokemonObject){
+        this.pokemonchildren.push(pokemonObject)
+    }
+
+    all(){
+        return this.pokemonchildren  
+    }
+
+    get(name){
+        return this.pokemonchildren(name)
+    }
+    
+    addPokemon(Pokemon){
+        th
+    }
+}
+
+
