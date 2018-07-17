98bb2b7ead071f3304ff3e37ac1906030cf156af
Nbeanavides23 <franco.noly@gmail.com>
2018-07-17 11:08:36 -0400

    now

--- main.js
+++ main.js
@@ -21,18 +21,19 @@ class Trainer {
         return this.pokemonchildren  
     }
 
-//     get(name) {
-//         return this.pokemonchildren.find(name) => {
-//             return element.name == name
-//         }
-//     }    
-    get(name){
-        return this.pokemonchildren(name)
-    
-    }  
+    get(name) {
+        return this.pokemonchildren.find(name) => {
+            return element.name == name
+        }
+    }    
     
 }
 
-// let Pokemon = new Trainer("superDavid") 
-// let poke = new Pokemon(2,3,2,Blaze)
+let Pokemon = new Trainer("superDavid") 
+let poke = new Pokemon(2,3,2,Blaze)
+
+axios.get("http://pokeapi.co/api/v2/pokemon${id}").then((response) => {
+    let data = response.data
+    let 
+})
 
