fec8946984fb5fadec8bb3ef6d721ac1141b3af9
Nbeanavides23 <franco.noly@gmail.com>
2018-07-17 16:07:08 -0400

    morejs

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 class Pokemon {
-    constructor(hp,attack,defense,abilities){
+    constructor(hp,attack,defense,abilities,photo,id,name){
         this.hp = hp;
         this.attack = attack;
         this.defense = defense;
         this.abilities = abilities;
+        this.photo = photo;
+        this.id = id;
+        this.name = name;
     }
 }
 
 class Trainer {
     constructor(name){
         this.name = name;
-        this.pokemonchildren = []
+        this.pokemonchildren = {} 
     }
     
     add(pokemonObject){
-        this.pokemonchildren.push(pokemonObject)
+        this.pokemonchildren[pokemonObject.name] = (pokemonObject)
     }
 
     all() { 
@@ -22,18 +25,28 @@ class Trainer {
     }
 
     get(name) {
-        return this.pokemonchildren.find(name) => {
+        return this.pokemonchildren.find(name => {
             return element.name == name
         }
-    }    
+        
+    )}    
     
 }
 
-let Pokemon = new Trainer("superDavid") 
-let poke = new Pokemon(2,3,2,Blaze)
+let PokemonTrainer = new Trainer("superDavid")
 
-axios.get("http://pokeapi.co/api/v2/pokemon${id}").then((response) => {
-    let data = response.data
-    let 
-})
+let api = function (pokemonid){
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonid}`).then((response) => {
+        let data = response.data
+        console.log(data)
+        // rename abi
+        let abi = new Pokemon(data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities,data.sprites.front_default,data.id,data.name)
+        
+        PokemonTrainer.add(abi)
 
+    }
+)}
+
+api(4)
+api(69)
+api(17)
