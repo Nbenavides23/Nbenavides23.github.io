ee947d3693346941bbb29128eb494c03c9a2798a
Nbeanavides23 <franco.noly@gmail.com>
2018-07-18 17:31:36 -0400

    updateNew

--- main.js
+++ main.js
@@ -9,16 +9,6 @@ class Pokemon {
         this.name = name;
     }
 
-
-    renderPoke(){
-        let div1 = document.createElement('div')
-        div1 .setAttribute('id','div1')
-        let pokeImg = document.createElement('img')
-        pokeImg.src = 'Image' + this.photo
-        // document.body.appendChild(pokeImg)
-        document.body.appendChild(div1)
-    }
-
 }
 
 class Trainer {
@@ -44,25 +34,39 @@ class Trainer {
     
 }
 
-let PokemonTrainer = new Trainer("superDavid")
+let pokemonTrainer = new Trainer("superDavid")
+let img = 1; // Global Variable 
 
 let showImg = function (pokemonid){
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonid}`).then((response) => {
+    axios.get(`https://pokeapi-nycda.firebaseio.com/pokemon/${pokemonid}.json`).then((response) => {
         
 
-        let data = response.data.sprites.front_default
-        console.log(data)
+        let data = response.data;
+        console.log(data);
+
+        let pokemon = new Pokemon(data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities,data.sprites.front_default,data.id,data.name)
+
        
+        // Adding the image calling from the API
+        let squareImg = document.getElementById(`SquareImg${img++}`)
+        let pokemonImg = document.createElement("img")
+        pokemonImg.setAttribute("id",`${pokemon.name}Img`)
+        pokemonImg.setAttribute("class","pokemonImg" )
+        pokemonImg.src = pokemon.photo;
+        // take the square and creating the image
+        squareImg.appendChild(pokemonImg)
+    
+        let text1 = document.getElementById(`Text${img++}`)
+        
+        text1.innerText = `Hp: ${pokemon.hp} 
+        Attack: ${pokemon.attack}
+        Defense: ${pokemon.defense}
+        Abilities: ${pokemon.abilities[0].ability.name}`;
 
 
-        let charmanderImg = document.createElement('img')
-        charmanderImg.src = data
-        document.body.appendChild(charmanderImg)
 
-
-        // let showImg = new Pokemon(data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities,data.sprites.front_default,data.id,data.name)
-
-        // PokemonTrainer.add(showImg)
+        pokemonTrainer.add(pokemon)
+       
          
         
 
@@ -70,5 +74,7 @@ let showImg = function (pokemonid){
 )}
 
 showImg(4)
+showImg(17)
+showImg(69)
 
 
