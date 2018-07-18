91e8b59dffa692e82f10d9bb81a86b1ffba86fe4
Nbeanavides23 <franco.noly@gmail.com>
2018-07-18 11:15:08 -0400

    update colors

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ class Pokemon {
         this.id = id;
         this.name = name;
     }
+
+
+    renderPoke(){
+        let div1 = document.createElement('div')
+        div1 .setAttribute('id','div1')
+        let pokeImg = document.createElement('img')
+        pokeImg.src = 'Image' + this.photo
+        // document.body.appendChild(pokeImg)
+        document.body.appendChild(div1)
+    }
+
 }
 
 class Trainer {
@@ -37,17 +48,27 @@ let PokemonTrainer = new Trainer("superDavid")
 
 let showImg = function (pokemonid){
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonid}`).then((response) => {
-        let data = response.data
+        
+
+        let data = response.data.sprites.front_default
         console.log(data)
        
-        let showImg = new Pokemon(data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities,data.sprites.front_default,data.id,data.name)
+
+
+        let charmanderImg = document.createElement('img')
+        charmanderImg.src = data
+        document.body.appendChild(charmanderImg)
+
+
+        // let showImg = new Pokemon(data.stats[5].base_stat,data.stats[4].base_stat,data.stats[3].base_stat,data.abilities,data.sprites.front_default,data.id,data.name)
+
+        // PokemonTrainer.add(showImg)
+         
         
-        PokemonTrainer.add(showImg)
 
     }
 )}
 
 showImg(4)
-showImg(69)
-showImg(17)
- 
+
+
