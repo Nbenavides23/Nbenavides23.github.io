80f27af0b33cce2ccc8741d64cd669929ead64e9
Nbeanavides23 <franco.noly@gmail.com>
2018-07-19 11:00:39 -0400

    merge

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
  
-let square1 = document.getElementById('Square1');
-let square3 = document.getElementById('Square3')
+// let square1 = document.getElementById('Square1');
+// let square3 = document.getElementById('Square3')
 // square1.addEventListener('click', ()=> {
-//     if(square3.style.display === 'none'){
-//         square3.style.display = 'block'
+//     if(square1.style.display === 'none'){
+//         square1.style.display = 'block'
 //     }else{
-//         square3.style.display = 'none;'
+//         square1.style.display = 'none;'
 //     }
 // })
