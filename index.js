e18368893975605b420127e38fef1f514af89de0
Nbeanavides23 <franco.noly@gmail.com>
2018-07-19 10:51:48 -0400

    none

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-{/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+//  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-$(document).ready(function(){
-    $(".btn-card").click(function(){
-
-        console.log("hello world");
-
-        ("pokemon").addClass(".is-active");
-
-
-
-
-
-    });
-}); */}
-
+ 
+let square1 = document.getElementById('Square1');
+let square3 = document.getElementById('Square3')
+// square1.addEventListener('click', ()=> {
+//     if(square3.style.display === 'none'){
+//         square3.style.display = 'block'
+//     }else{
+//         square3.style.display = 'none;'
+//     }
+// })
