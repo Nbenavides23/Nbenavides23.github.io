c3a471f3d187189a70e372ab7d0f1247d59a9baf
Nbeanavides23 <franco.noly@gmail.com>
2018-07-19 12:00:24 -0400

    added on click events

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 //  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
  
-// let square1 = document.getElementById('Square1');
-// let square3 = document.getElementById('Square3')
-// square1.addEventListener('click', ()=> {
-//     if(square1.style.display === 'none'){
-//         square1.style.display = 'block'
-//     }else{
-//         square1.style.display = 'none;'
-//     }
-// })
+let square1 = document.getElementById('Square1');
+let square3 = document.getElementById('Square3');
+let square2 = document.getElementById('Square2');
+let text2 = document.getElementById('Text2');
+let text1 = document.getElementById('Text1');
+let squareImg2 = document.getElementById('SquareImg2');
+let squareImg1 = document.getElementById('SquareImg1');
+square3.addEventListener('click', (e)=> {
+    if(square3.style.display === 'block'){
+        square3.style.display = 'none'
+        text2.style.opacity = 1;
+        squareImg2.style.opacity = 1;
+
+    }else{
+        square3.style.display = 'block'
+    }
+
+})
+
+square2.addEventListener('click', (e)=> {
+    if(square2.style.display === 'block'){
+        square2.style.display = 'none'
+        text1.style.opacity = 1;
+        squareImg1.style.opacity = 1;
+    }else{
+        square2.style.display = 'block'
+    }
+
+})
+
+square1.addEventListener('click', (e)=> {
+    if(square1.style.display === 'block'){
+        square1.style.display = 'none'
+    }else{
+        square1.style.display = 'block'
+    }
+
+})
+
+let ball = document.getElementById('ball')
+ball.addEventListener('click', () =>{
+    window.location.reload()
+})
